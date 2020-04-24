f4df1f65d80f779fe5eca6189a17d8afe4296f1e
prog815 <eavprog@gmail.com>
2020-04-24 10:35:28 +0500

    Create abscur.py

---
+++ abscur.py
@@ -0,0 +1,4 @@
+""" Библиотека с методами для доступа к данным проекта <Абсолютный курс> """
+
+src_url = "https://docs.google.com/spreadsheets/d/1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs/export?format=csv&id=1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs&gid=422799220"
+
