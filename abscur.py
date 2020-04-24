7897d614346ebeec1eb78aab9df35953fbe31084
prog815 <eavprog@gmail.com>
2020-04-24 11:10:15 +0500

    Update abscur.py

--- abscur.py
+++ abscur.py
@@ -2,11 +2,12 @@
 
 src_url = "https://docs.google.com/spreadsheets/d/1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs/export?format=csv&id=1_-cdNCIC6NgyRDj0zLJBaohXo-K8qCfxfDLbYq2JLhs&gid=422799220"
 
-__abs_data__ = None
-
 import pandas as pd
 
+__abs_data__ = None
+
 def get_data():
+    global __abs_data__
     if __abs_data__ is None:
         __abs_data__ = pd.read_csv(src_url,decimal=',',parse_dates=True,index_col=0)
     return __abs_data__
